6e6ceaa894a1a30af5197080e563792b52fd361e
Noah <40500696+NoahStricker@users.noreply.github.com>
2022-11-12 21:18:54 -0700

    Finish calibration and rename class

--- core/vision/camera_calibration.py
+++ core/vision/camera_calibration.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_calib3d/py_calibration/py_calibration.html
-class UserCameraCalibration:
+class CameraCalibration:
 
     # Define the chess board rows and columns
     rows: int
@@ -24,7 +24,22 @@ class UserCameraCalibration:
         self.objectPoints[:, :2] = np.mgrid[0:rows, 0:columns].T.reshape(-1, 2)*21.757
 
     def show_pattern(self, img):
-        pass
+        '''Draw calibration grid on the image and return true if detected.'''
+        # Convert to gray scale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (self.rows, self.columns), None)
+
+        # Make sure the chess board pattern was found in the image
+        if ret:
+            # Refine the corner position
+            corners = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), self.criteria)
+
+            # Draw the corners on the image
+            cv2.drawChessboardCorners(img, (self.rows, self.columns), corners, ret)
+
+        return (ret, img)
     
     def add_to_calibration(self, img):
 
@@ -48,3 +63,9 @@ class UserCameraCalibration:
 
         # Should return true if adding to calibration was successful.
         return ret
+
+    def finish_calibration(self, imageSize):
+        return cv2.calibrateCamera(self.objectPointsArray, self.imgPointsArray, imageSize, None, None)
+
+def get_image_size(img):
+    return img.shape[::-1]

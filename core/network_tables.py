152105dccf6bcb96d3c8e9e668b487e869238e11
Noah <40500696+NoahStricker@users.noreply.github.com>
2023-01-28 16:14:17 -0700

    Delete old files

--- core/network_tables.py
+++ core/network_tables.py
@@ -2,7 +2,6 @@ import time
 from networktables import NetworkTables
 
 
-NetworkTables.getDefault().initialize(server='10.20.83.2')
 NetworkTables.initialize(server='10.20.83.2')
 print("NT Client Established")
 
--- core/vision/readme.md
+++
@@ -1,1 +0,0 @@
-This directory contains the bulk of the vision pipeline. 

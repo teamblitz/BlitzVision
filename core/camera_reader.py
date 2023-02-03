afe329401b02c134976ac37dfdf92410c9d370a7
teamblitz2083 <github@teamblitz.net>
2023-02-02 18:22:53 -0700

    Quad cam working on jetson

--- core/camera_reader.py
+++ core/camera_reader.py
@@ -1,5 +1,5 @@
 from threading import Thread
-from typing import Callable, Tuple
+from typing import Callable, Tuple, Any
 import cv2
 
 ListenerFunction = Callable[[Tuple[cv2.Mat, ...]], None]
@@ -7,4 +7,5 @@ ListenerFunction = Callable[[Tuple[cv2.Mat, ...]], None]
 ## Abstract camera reader class
 class CameraReader(Thread):
     def __init__(self, listener: ListenerFunction):
+        super().__init__()
         self.listener = listener
--- core/quad_camera_reader.py
+++ core/quad_camera_reader.py
@@ -1,20 +1,74 @@
 import cv2
+import time
+import os
+import subprocess
 
 from camera_reader import CameraReader, ListenerFunction
+
+
+def resize(frame, dst_width):
+    width = frame.shape[1]
+    height = frame.shape[0]
+    scale = dst_width * 1.0 / width
+    return cv2.resize(frame, (int(scale * width), int(scale * height)))
 
 class QuadCameraReader(CameraReader):
 
     def __init__(self, listener: ListenerFunction):
-        super.__init__(listener)
+        super().__init__(listener)
         
         self.cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
 
-        self.cap.set(cv2.CAP_PROP_FOURCC, None) # TODO: Set pixel format
+        # Set the pixel format for BA81
+        self.cap.set(cv2.CAP_PROP_FOURCC, 825770306)
+        
+
+        # Disable rgb conversion as this makes the fps 15x slower when on.
+        self.cap.set(cv2.CAP_PROP_CONVERT_RGB, 0)
+        
+        # Size 5120x800
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 5120)
+
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 800)
+
+
+        #Set camera exposure, we need to fetch a frame first for it to work.
+        self.cap.read()
+        print(subprocess.run(["v4l2-ctl -c exposure=500"], shell=True))
+    
+
+
 
     def run(self):
+        frame_count = 0
+        start = time.time()
+        
+    
         while True:
-            # Wait for frame
-            # Seperate frames
-            # Inform the listener
-            break
+            ret, frame = self.cap.read()
 
+            # We have to do this because convert rgb is off.
+            w = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+            h = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+            frame = frame.reshape(int(h), int(w))
+
+            # Convert to rby
+            frame = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2BGR)
+
+            # resize for display TODO Remove later
+            # frame = resize(frame, 1280.0)
+            
+            # cv2.imshow("Test", frame)
+            # if cv2.waitKey(1) == ord('q'):
+            #     break
+            # frame_count +=1
+            # if time.time() - start >= 1:
+            #     print(f"FPS: {frame_count}")
+            #     start = time.time()
+            #     frame_count = 0 
+        # self.cap.release()
+            
+
+if __name__ == "__main__":
+    reader = QuadCameraReader(None)
+    reader.run()

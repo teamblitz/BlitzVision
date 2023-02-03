3f0f35b4564dc4b6d926f3b64027c633ded31a04
teamblitz2083 <github@teamblitz.net>
2023-02-02 19:32:19 -0700

    Tag tracking start

--- core/dispatcher.py
+++ core/dispatcher.py
@@ -1,8 +1,33 @@
-from threading import Thread
+from quad_camera_reader import QuadCameraReader
+
+from typing import List, Dict, Any, Union
+from threading import Event
+from queue import Queue
 
 
-class Dispatcher(Thread):
+class Dispatcher:
 
 
-    def run():
-        pass
+
+    def __init__(self):
+        self.quadCamera = QuadCameraReader((lambda x: self.listener(0, x)))
+
+
+        self.cameraPipelines: List[Dict[str, Any, Union[Event, Queue]]] = [
+            {
+                "tagQueue": Queue(),
+                "tagBusyEvent": Event(),
+                "tagSettings": [
+                    1
+                ],
+                "yoloQueue": None,
+                "yoloBusyEvent": None
+            }
+        ]
+
+    def listener(self, id, frames):
+        if not self.cameraPipelines[id]["tagBusyEvent"].is_set():
+            # Give it the frame/frames
+            self.cameraPipelines[id]["tagQueue"].put(frames)
+            # Mark as busy
+            self.cameraPipelines[id]["tagBusyEvent"].set()
